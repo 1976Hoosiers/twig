06be4ac79edc8a3c2f39fd1be3219958229cd6f1
Rabah Rahil <rabahrahil@gmail.com>
2017-09-25 09:14:27 -0500

    Removed API Keys...dope

--- send-sms.js
+++ send-sms.js
@@ -1,14 +1,14 @@
 // Twilio Credentials 
 var accountSid = 'AC368afb7545f5c85c1e85f67d5f417573'; 
-var authToken = 'eb7f3039336bf151266238455fa45bed'; 
+var authToken = 'insert key'; 
  
 //require the Twilio module and create a REST client 
 var client = require('twilio')(accountSid, authToken); 
  
 client.messages.create({ 
-    to: "+13173586459", 
+    to: "+17372224053", 
     from: "+15125999970", 
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?", 
+    body: "Rabah's makes me so happy. Rabah is the best. I am so lucky to have him.", 
 }, function(err, message) { 
     console.log(message.sid); 
 });
